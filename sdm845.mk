e563c04fc79d9d00879926b46ba59a1a771b07de
AxelBlaz3 <karthikgaddam4@gmail.com>
2019-08-28 16:11:09 +0530

    sdm845.mk: Build ThermalController
    Signed-off-by: AxelBlaz3 <karthikgaddam4@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -282,6 +282,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service \
     thermal.sdm845
+	
+# ThermalController app
+PRODUCT_PACKAGES += \
+    ThermalController	
 
 # Trust HAL
 PRODUCT_PACKAGES += \
