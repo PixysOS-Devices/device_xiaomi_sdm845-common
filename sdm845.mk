ba4443e9cab7ef36bc6b5068909f28d720b987e1
AxelBlaz3 <karthikgaddam4@gmail.com>
2019-09-17 20:02:41 +0530

    sdm845-common: Nuke ThermalController
    This reverts the following commits:
    • f6e55eabcdf36e54330b6684f223d59796e5c01f
    • e563c04fc79d9d00879926b46ba59a1a771b07de
    • 1e85d1de2899374984b2c3c6b112daaddb9ebe12
    
    Signed-off-by: AxelBlaz3 <karthikgaddam4@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -284,10 +284,6 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service \
     thermal.sdm845
-	
-# ThermalController app
-PRODUCT_PACKAGES += \
-    ThermalController	
 
 # Trust HAL
 PRODUCT_PACKAGES += \
